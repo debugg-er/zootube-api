9e5d625827428ea330a2807701c6866b48f1ee19
backy4rd <khainguyen9209@gmail.com>
2021-03-14 16:48:29 +0700

    create searchUsers

--- src/controllers/search_controller.ts
+++ src/controllers/search_controller.ts
@@ -1,7 +1,8 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 
 import { Video } from "../entities/Video";
+import { User } from "../entities/User";
 import asyncHandler from "../decorators/async_handler";
 import { isDateFormatIfExist, isNumberIfExist, mustExist } from "../decorators/validate_decorators";
 import { mustInRangeIfExist } from "../decorators/assert_decorators";
@@ -13,7 +14,7 @@ class SearchController {
     @isNumberIfExist("query.offset", "query.limit", "query.min_duration")
     @mustInRangeIfExist("query.offset", 0, Infinity)
     @mustInRangeIfExist("query.limit", 0, 100)
-    public async searchVideos(req: Request, res: Response, next: NextFunction) {
+    public async searchVideos(req: Request, res: Response) {
         const q = req.query.q as string;
         const max_upload_date = req.query.max_upload_date as string;
         const min_duration = req.query.min_duration as string;
@@ -62,6 +63,33 @@ class SearchController {
 
         res.status(200).json({
             data: videos,
+        });
+    }
+
+    @asyncHandler
+    @mustExist("query.q")
+    @isNumberIfExist("query.offset", "query.limit")
+    @mustInRangeIfExist("query.offset", 0, Infinity)
+    @mustInRangeIfExist("query.limit", 0, 100)
+    public async searchUsers(req: Request, res: Response) {
+        const q = req.query.q as string;
+        const offset = +req.query.offset || 0;
+        const limit = +req.query.limit || 30;
+
+        const wildcardQuery = `%${q.toLowerCase()}%`;
+
+        let users = await getRepository(User)
+            .createQueryBuilder("users")
+            .loadRelationCountAndMap("users.subscribers", "users.subscribers")
+            .select(["users.firstName", "users.lastName", "users.username", "users.iconPath"])
+            .where("LOWER(username) LIKE :q", { q: wildcardQuery })
+            .orWhere("LOWER(CONCAT(first_name, ' ', last_name)) LIKE :q", { q: wildcardQuery })
+            .skip(offset)
+            .take(limit)
+            .getMany();
+
+        res.status(200).json({
+            data: users,
         });
     }
 }
--- src/routes/search_route.ts
+++ src/routes/search_route.ts
@@ -5,5 +5,6 @@ import searchController from "../controllers/search_controller";
 const router = express.Router();
 
 router.get("/videos", searchController.searchVideos);
+router.get("/users", searchController.searchUsers);
 
 export default router;
