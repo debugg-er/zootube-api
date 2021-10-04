91e5287ab87a216a6cd70371926c3d601d98191c
backy4rd <khainguyen9209@gmail.com>
2021-10-04 16:14:30 +0700

    create stream entity

---
+++ src/entities/Stream.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, Index, JoinColumn, OneToOne } from "typeorm";
+import { User } from "./User";
+
+@Index("streams_pkey", ["id"], { unique: true })
+@Entity("streams", { schema: "public" })
+export class Stream {
+    @Column("character", { primary: true, name: "id", length: 10 })
+    id: string;
+
+    @Column("character", { name: "stream_key", length: 32, select: false })
+    streamKey: string;
+
+    @Column("character varying", { name: "name", length: 128 })
+    name: string;
+
+    @Column("character varying", { name: "thumbnail_path", length: 128 })
+    thumbnailPath: string | null;
+
+    @Column("boolean", { name: "is_streaming", default: false })
+    isStreaming: boolean;
+
+    @Column("integer", { name: "user_id", select: false })
+    userId: number;
+
+    @OneToOne(() => User, (users) => users.stream)
+    @JoinColumn([{ name: "user_id", referencedColumnName: "id" }])
+    user: User;
+}
