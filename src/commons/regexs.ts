50ef351eafabd773a110b43ee3cfd9d15edaa098
backy4rd <khainguyen9209@gmail.com>
2021-10-15 17:13:44 +0700

    fix username length conflit with admin account

--- src/commons/regexs.ts
+++ src/commons/regexs.ts
@@ -2,7 +2,7 @@ const vietnameseCharacters =
     "ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ";
 
 export const nameRegex = new RegExp(`^(?! )[ a-zA-Z${vietnameseCharacters}]+(?<! )$`);
-export const usernameRegex = /^[a-zA-Z0-9][a-zA-Z0-9\._]{5,31}$/;
+export const usernameRegex = /^[a-zA-Z0-9][a-zA-Z0-9\._]{4,31}$/;
 export const passwordRegex = /^\S{6,32}$/;
 export const urlPathRegex = /^(\/[a-zA-Z0-9._-]+)+\b$/;
 export const jwtRegex = /^[A-Za-z]+ [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/;
