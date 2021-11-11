e37e00568fdb56eaa035ba18e235e1606ba92613
backy4rd <khainguyen9209@gmail.com>
2021-11-11 18:40:53 +0700

    add delete login log

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -12,9 +12,6 @@ router.use(express.urlencoded({ extended: true }));
 // login
 router.post("/login", authController.login);
 
-// get login logs
-router.get("/logs", authMiddleware.authorize, authController.getLoginLogs);
-
 // logout
 router.post("/logout", authMiddleware.authorize, authController.logout);
 
@@ -23,5 +20,14 @@ router.post("/register", authController.register);
 
 // reset password
 router.post("/reset", authMiddleware.authorize, authController.changePassword);
+
+// get login logs
+router.get("/logs", authMiddleware.authorize, authController.getLoginLogs);
+
+// delete login log by id
+router.delete("/logs/:log_id(\\d+)", authMiddleware.authorize, authController.deleteDevice);
+
+// delete all login log
+router.delete("/logs", authMiddleware.authorize, authController.deleteAllOtherDevices);
 
 export default router;
