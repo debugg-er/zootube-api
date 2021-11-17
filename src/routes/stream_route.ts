6d1410b92b9e262d33bc2d1fbcfa9fa940fe3ab5
backy4rd <khainguyen9209@gmail.com>
2021-11-17 19:23:50 +0700

    add upload streamed video endpoint

--- src/routes/stream_route.ts
+++ src/routes/stream_route.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 
 import streamController from "../controllers/stream_controller";
+import multipartMiddleware from "../middlewares/multipart_middleware";
 
 const router = express.Router();
 
@@ -13,9 +14,17 @@ router.get("/", streamController.getStreams);
 // get stream
 router.get("/:stream_id(\\w{10})", streamController.getStream);
 
-// update stream status with stream_key (live or off)
-// Note: this route should be filtered by webserver
+// Note: these endpoit under this line should be filtered by webserver
 // and only allow stream-server to request locally
+
+// update stream status with stream_key (live or off)
 router.patch("/:stream_id(\\w{10})", streamController.updateStreamStatus);
+
+// upload streamed video
+router.post(
+    "/",
+    multipartMiddleware.storeUploadFiles("video"),
+    streamController.uploadStreamedVideo,
+);
 
 export default router;
