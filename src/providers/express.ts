d2c4aa4e72c5982a706cfe722cc9133752d9b519
Khai Nguyen <khainguyen9209@gmail.com>
2023-11-05 00:49:59 +0700

    add cors

--- src/providers/express.ts
+++ src/providers/express.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as morgan from "morgan";
+import * as cors from "cors";
 import env from "./env";
 
 import searchRoute from "../routes/search_route";
@@ -22,6 +23,7 @@ if (env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
+app.use(cors())
 app.use((req, res, next) => {
     req.local = {};
     next();
